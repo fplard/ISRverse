7a5217016f55a61d4a06b710903998615d51fb0c
fplard <floriane.c.plard@gmail.com>
2024-04-26 07:47:38 +0200

    Outlier detecting in weights
    First commit

---
+++ R/zzz.R
@@ -0,0 +1,2 @@
+globalVariables(c("Age", "AnonID", "MeasurementValue", "age0", "agem", "ager", "anonid", "keep2", "keep3",
+    "lwr", "nb", "pop", "pred", "pred1", "pred2", "res"), package ="ISRverse")
