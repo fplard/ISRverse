959e2d1fa65fe822af2a448536feae9db0b7ad0a
fplard <floriane.c.plard@gmail.com>
2024-05-03 09:46:22 +0200

    gro_cleanmeasures
    added

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,4 @@
-globalVariables(c("Age", "AnonID", "MeasurementValue", "age0", "agem", "ager", "anonid", "keep2", "keep3",
-    "lwr", "nb", "pop", "pred", "pred1", "pred2", "res"), package ="ISRverse")
+globalVariables(c("Age", "anonID", "MeasurementValue", "age0", "agem", "ager", "anonid", "keep2", "keep3",
+    "lwr", "nb", "pop", "pred", "pred1", "pred2", "res", 'birthType', 'Sex','cond_captivebirth',
+    'cond_sex','MeasurementDate','MeasurementType', 'CollectionScopeType','UnitOfMeasure',
+    'ExcludedFromNorms','EstimatedMeasurement','RecordType','BirthDate','age','toKg', 'la'), package ="ISRverse")
